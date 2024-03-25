81e3e364690419eb4acd6c3801b04f5cbdf91023
ESousa97 <xfuuuuuuuuury2018@gmail.com>
2024-03-25 16:30:25 -0300

    att rotas

--- pages/procedimentos/[id].js
+++ pages/procedimentos/[id].js
@@ -20,7 +20,7 @@ const ProcedureDetails = () => {
       setLoading(true);
   
       try {
-        const response = await fetch(`https://server-json-eight.vercel.app/api/procedure`);
+        const response = await fetch(`https://server-json-eight.vercel.app/api/procedure.js/${id}`);
   
         if (!response.ok) {
           throw new Error(`Erro na requisição: ${response.status} ${response.statusText}`);
@@ -44,7 +44,7 @@ const ProcedureDetails = () => {
     if (!content) return ''; // Se o conteúdo estiver vazio, retornar uma string vazia
 
     // Processar o conteúdo antes de renderizar com Markdown
-    let processedContent = content.replace(/\/Assets\/(IMC\d+\/IMC\d+__[1-9].png)/g, 'https://esdatabase.vercel.app/$1');
+    let processedContent = content.replace(/\/Assets\/(IMC\d+\/IMC\d+__[1-9].png)/g, 'esdatabase.vercel.app/$1');
     processedContent = DOMPurify.sanitize(processedContent); // Sanitizar o conteúdo para evitar XSS
     return processedContent;
   };
