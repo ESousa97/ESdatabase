bf80127c6872022c58e6212fc169df77ee60a84f
ESousa97 <xfuuuuuuuuury2018@gmail.com>
2024-04-08 11:29:24 -0300

    adicionado o meta tag no index

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,21 @@
-import React, { useEffect } from "react"; // Correção aqui
+import React, { useEffect } from "react";
 import { useRouter } from "next/router";
+import Head from "next/head"; // Importar o Head de next/head
 
 const Index = () => {
   const router = useRouter();
 
   useEffect(() => {
-    router.push("./login");
+    router.push("/login"); // Correção no caminho
   }, [router]);
 
-  return null;
+  return (
+    <>
+      <Head>
+        <meta name="google-site-verification" content="UvuKX1cPOo1fakawbq5Ry3zxnRuJdHQPdfHTLn4pXGY" />
+      </Head>
+    </>
+  );
 };
 
 export default Index;
