377181bc4d77675abd8c3e367ae7862eeb80c4f9
Sousa <114963739+ESousa97@users.noreply.github.com>
2024-07-04 17:07:20 -0300

    adicionado modo claro, e modo escuro, no themeprovider e mainlayout.

--- componentes/ThemeProvider/ThemeProvider.js
+++ componentes/ThemeProvider/ThemeProvider.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect, createContext, useContext } from 'react';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-const theme = createTheme({
+const ThemeContext = createContext();
+
+export const useCustomTheme = () => useContext(ThemeContext);
+
+const getDesignTokens = (mode) => ({
   palette: {
+    mode,
     primary: {
-      main: '#1976d2',
+      main: mode === 'dark' ? '#0a43ff' : '#1976d2',
     },
     secondary: {
-      main: '#dc004e',
+      main: mode === 'dark' ? '#f48fb1' : '#dc004e',
+    },
+    background: {
+      default: mode === 'dark' ? '#0a43ff' : '#f0f0f0',
+      paper: mode === 'dark' ? '#112240' : '#ffffff',
     },
     text: {
-      primary: '#333333',
+      primary: mode === 'dark' ? '#ffffff' : '#333333',
+      secondary: mode === 'dark' ? '#cccccc' : '#666666',
     },
   },
   typography: {
@@ -20,7 +30,31 @@ const theme = createTheme({
 });
 
 const CustomThemeProvider = ({ children }) => {
-  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+  const [darkMode, setDarkMode] = useState(() => {
+    if (typeof window !== 'undefined') {
+      const savedMode = localStorage.getItem('darkMode');
+      return savedMode ? savedMode === 'true' : true; // default to dark mode
+    }
+    return true; // Um valor padrão para quando estiver no lado do servidor
+  });
+
+  useEffect(() => {
+    localStorage.setItem('darkMode', darkMode.toString());
+  }, [darkMode]);
+
+  const theme = createTheme(getDesignTokens(darkMode ? 'dark' : 'light'));
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+
+  return (
+    <ThemeContext.Provider value={{ toggleDarkMode }}>
+      <ThemeProvider theme={theme}>
+        {children}
+      </ThemeProvider>
+    </ThemeContext.Provider>
+  );
 };
 
 export default CustomThemeProvider;
--- pages/MainLayout.js
+++ pages/MainLayout.js
@@ -1,53 +1,62 @@
-import React, { useState } from 'react';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
+import React, { useState, useEffect } from 'react';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { AppBar, Toolbar, IconButton, Typography, CssBaseline, Drawer, Button, Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import HomeIcon from '@mui/icons-material/Home';
 import LogoutIcon from '@mui/icons-material/Logout';
-import Typography from '@mui/material/Typography';
-import CssBaseline from '@mui/material/CssBaseline';
-import Drawer from '@mui/material/Drawer';
-import { useTheme, ThemeProvider, createTheme } from '@mui/material/styles';
+import Brightness4Icon from '@mui/icons-material/Brightness4';
+import Brightness7Icon from '@mui/icons-material/Brightness7';
 import SearchBox from '../componentes/SearchBox/SearchBox';
 import SideMenu from '../componentes/SideMenu/SideMenu';
 import { useRouter } from 'next/router';
 import { signOut } from 'next-auth/react';
-import Dialog from '@mui/material/Dialog';
-import DialogTitle from '@mui/material/DialogTitle';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogActions from '@mui/material/DialogActions';
-import Button from '@mui/material/Button';
 import Head from 'next/head';
 
 const drawerWidth = 320; // Valor em pixels, ajuste conforme necessário
 
-const darkTheme = createTheme({
+const getDesignTokens = (mode) => ({
   palette: {
-    mode: 'dark',
+    mode,
     primary: {
-      main: '#90caf9',
+      main: mode === 'dark' ? '#1e40af' : '#1976d2', // Azul mais claro para o modo escuro, e azul adequado para o claro
     },
     secondary: {
-      main: '#f48fb1',
+      main: mode === 'dark' ? '#ec407a' : '#d32f2f', // Rosa brilhante para o escuro, vermelho mais escuro para o claro
     },
     background: {
-      default: '#121212',
-      paper: '#1e1e1e',
+      default: mode === 'dark' ? '#121212' : '#f0f0f0', // Fundo mais escuro para o modo escuro
+      paper: mode === 'dark' ? '#112240' : '#ffffff', // cor dos cards, letras e toolbar
     },
     text: {
-      primary: '#ffffff',
-      secondary: '#aaaaaa',
+      primary: mode === 'dark' ? '#ffffff' : '#1e1e1e', // Branco para o escuro, preto para o claro
+      secondary: mode === 'dark' ? '#b3b3b3' : '#4f4f4f', // Cinza mais claro para o escuro, cinza escuro para o claro
     },
+  },
+  typography: {
+    fontFamily: 'Roboto, "Helvetica Neue", Arial, sans-serif',
+    fontWeightBold: 700,
   },
 });
 
 const MainLayout = ({ children }) => {
+  const [darkMode, setDarkMode] = useState(() => {
+    if (typeof window !== 'undefined') {
+      const savedMode = localStorage.getItem('darkMode');
+      return savedMode ? savedMode === 'true' : true; // default to dark mode
+    }
+    return true; // fallback default to dark mode if not in browser environment
+  });
   const [open, setOpen] = useState(false);
-  const [logoutDialogOpen, setLogoutDialogOpen] = useState(false); // Estado para controlar a abertura do diálogo de logout
-  const theme = useTheme();
+  const [logoutDialogOpen, setLogoutDialogOpen] = useState(false);
   const router = useRouter();
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      localStorage.setItem('darkMode', darkMode.toString());
+    }
+  }, [darkMode]);
+
+  const theme = createTheme(getDesignTokens(darkMode ? 'dark' : 'light'));
 
   const handleDrawerToggle = () => {
     setOpen(!open);
@@ -58,7 +67,7 @@ const MainLayout = ({ children }) => {
   };
 
   const handleLogout = () => {
-    setLogoutDialogOpen(true); // Abrir o diálogo de logout
+    setLogoutDialogOpen(true);
   };
 
   const handleConfirmLogout = async () => {
@@ -74,45 +83,34 @@ const MainLayout = ({ children }) => {
     setOpen(false);
   };
 
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+
   return (
     <>
       <Head>
         <meta name="google-site-verification" content="UvuKX1cPOo1fakawbq5Ry3zxnRuJdHQPdfHTLn4pXGY" />
       </Head>
-      <ThemeProvider theme={darkTheme}>
+      <ThemeProvider theme={theme}>
         <CssBaseline />
         <AppBar position="fixed" sx={{ zIndex: theme.zIndex.drawer + 1 }}>
           <Toolbar>
-            <IconButton
-              color="inherit"
-              aria-label="open drawer"
-              onClick={handleDrawerToggle}
-              size="large"
-            >
+            <IconButton color="inherit" aria-label="open drawer" onClick={handleDrawerToggle} size="large">
               <MenuIcon />
             </IconButton>
-            <IconButton
-              color="inherit"
-              aria-label="home"
-              onClick={handleHomeClick}
-              size="large"
-            >
+            <IconButton color="inherit" aria-label="home" onClick={handleHomeClick} size="large">
               <HomeIcon />
             </IconButton>
-            {/* Botão de logout */}
-            <IconButton
-              color="inherit"
-              aria-label="logout"
-              onClick={handleLogout}
-              size="large"
-              style={{ transform: 'rotate(-180deg)' }} // Adiciona a rotação
-            >
+            <IconButton color="inherit" aria-label="toggle theme" onClick={toggleDarkMode} size="large">
+              {darkMode ? <Brightness7Icon /> : <Brightness4Icon />}
+            </IconButton>
+            <IconButton color="inherit" aria-label="logout" onClick={handleLogout} size="large" style={{ transform: 'rotate(-180deg)' }}>
               <LogoutIcon />
             </IconButton>
             <Typography variant="h6" noWrap style={{ flexGrow: 1, marginLeft: '12px' }}>
               Data Base
             </Typography>
-            {/* Spacer para espaço antes do título */}
             <div style={{ flexGrow: 1 }} />
             <SearchBox />
           </Toolbar>
@@ -134,26 +132,17 @@ const MainLayout = ({ children }) => {
         <main
           style={{
             flexGrow: 1,
-            padding: theme.spacing(7),
+            padding: theme.spacing(7), //controle de espaço do conteudo dentro dos processos.
+            marginLeft: open ? drawerWidth : 0,
             transition: theme.transitions.create('margin', {
               easing: theme.transitions.easing.sharp,
               duration: theme.transitions.duration.leavingScreen,
-            }),
-            marginLeft: 0,
-            ...(open && {
-              transition: theme.transitions.create('margin', {
-                easing: theme.transitions.easing.easeOut,
-                duration: theme.transitions.duration.enteringScreen,
-              }),
-              marginLeft: drawerWidth,
-              width: `calc(100% - ${drawerWidth}px)`,
             }),
           }}
         >
           {children}
         </main>
         
-        {/* Diálogo de confirmação de logout */}
         <Dialog
           open={logoutDialogOpen}
           onClose={handleCloseLogoutDialog}
